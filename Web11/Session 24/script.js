a2b3e937e16a87a9766e60e82e723cdf07d53d1a
MortezaGhoddousi <djmgh1994@gmail.com>
2023-08-30 23:43:31 +0330

    Added session 24 JS and HTML codes

---
+++ Web11/Session 24/script.js
@@ -0,0 +1,80 @@
+// var obj = {
+//     name: "Morteza",
+//     age: 29,
+//     sex: "male",
+//     height: 185
+// };
+
+// console.log(obj["name"]);
+// console.log(obj.age);
+// console.log(obj);
+
+
+// function person(name, age){
+//     this.name = name;
+//     this.age = age;
+//     this.changeName = function(name){
+//         this.name = name;
+//     }
+// }
+
+// var p = new person("Morteza", 29);
+
+// console.log(p.name);
+// console.log(p.age);
+
+// p.changeName("Parsa");
+
+// console.log(p.name);
+
+
+// function person(name, age){
+//     this.name = name;
+//     this.age = age;
+//     this.changeName = Change;
+// }
+
+// function Change(name){
+//     this.name = name;
+// }
+
+// var p = new person("Morteza", 29);
+
+// console.log(p.name);
+// console.log(p.age);
+
+// p.changeName("Parsa");
+
+// console.log(p.name);
+
+// console.log(Math.PI);
+
+// console.log(Math.round(Math.PI));
+
+// var arr = [3, 4, 1, 10, 7];
+// console.log(arr);
+
+// console.log(Math.min(3, 4, 1, 10, 7));
+// console.log(Math.max(3, 4, 1, 10, 7));
+
+// var a = 2
+// var b = 4
+// var x = a+(b-a)*Math.random();
+
+// console.log(x);
+
+
+
+// function Clock(){
+//     var d = new Date();
+//     document.body.innerHTML = (`${d.getHours()}: ${d.getMinutes()}: ${d.getSeconds()}`);
+// }
+
+// setInterval(Clock, 1000);
+
+
+
+
+
+
+

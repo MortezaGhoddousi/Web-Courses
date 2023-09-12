aa4733523f6544276fd48a2b3b9c18250721da14
MortezaGhoddousi <143504966+MortezaGhoddousi@users.noreply.github.com>
2023-09-12 18:25:18 +0330

    Added session 13 to Web 14

---
+++ Web14/Session 13/index.php
@@ -0,0 +1,57 @@
+<?php
+
+// $name = "Morteza";
+// echo $name;
+
+// $x = 20;
+// $y = "30";
+
+// echo "$x + $y = ";
+// echo $x + $y;
+// echo "<br>";
+// echo "$x - $y = ";
+// echo $x - $y;
+// echo "<br>";
+// echo "$x * $y = ";
+// echo $x * $y;
+// echo "<br>";
+// echo "$x / $y = ";
+// echo $x / $y;
+// echo "<br>";
+// echo "$x % $y = ";
+// echo $x % $y;
+// echo "<br>";
+
+// $x = $x + 10;
+
+// $c = $x != $y;
+// echo $c; 
+
+// $AmirALi = 19;
+// $Sajjad = 12;
+// $Alireza = 16;
+// $Yashar = 19;
+// $Nima = 18;
+// $Amir = 16;
+// $AmirHossein = 20;
+// $Younes = 19;
+
+// $sum = $AmirALi + $Sajjad + $Alireza + $Yashar + $Nima + $Amir + $AmirHossein + $Younes;
+// $average = $sum / 8;
+// echo $average; 
+
+$Names = array("AmirAli", "Sajjad", "Alireza", "Yashar", "Nima", "Amir", "Amirhoseyn", "Younes");
+$Scores = array(19, 12, 16, 19, 18, 16, 20, 19);
+echo $Names[6], $Names[7];
+echo $Scores[6];
+
+
+
+// $Scores = [19, 12, 16, 19, 18, 16, 20, 19, 20, 21, 14];
+// $sum = 0;
+// for($i=0; $i<sizeof($Scores); $i++){
+//     $sum+=$Scores[$i];
+// }
+// echo $sum/sizeof($Scores);
+
+?>

7bb86bdc34fc23e847454b3740a4145bc0ce9c14
MortezaGhoddousi <143504966+MortezaGhoddousi@users.noreply.github.com>
2023-09-17 17:48:59 +0330

    Added session 14 to web 14

---
+++ Web14/Session 14/index.php
@@ -0,0 +1,42 @@
+<?php
+
+// // If statements
+
+// $container = array ("name"=>array("Younes", "Nima", "Yashar", "Alireza", "Sajjad", "Amir"),
+//                     "age"=>array(17, 15, 15, 17, 19, 18),
+//                     "score"=>array(19.5, 18.5, 1, 16, 9, 16));
+
+// if ($container["score"][4] > 10){
+//     $name = $container["name"][4];
+//     echo "$name passed";
+// }
+// elseif ($container["score"][4] < 10 && $container["score"][4] > 2) {
+//     $name = $container["name"][4];
+//     echo "$name failed with second change";
+// }
+// else {
+//     $name = $container["name"][4];
+//     echo "$name failed";
+// }
+
+// // For loops
+
+// for ($i=0; $i<3; $i++){
+//     echo $i;
+//     echo "<br/>";
+//     echo "Hello";
+//     echo "<br/>";
+// }
+
+// // While loops
+
+$i = 0;
+while ($i<100){
+    echo $i;
+    echo "<br/>";
+    echo "Hello World!";
+    echo "<br>";
+    $i++;
+}
+
+?>

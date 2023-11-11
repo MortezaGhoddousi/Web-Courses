8cec2db5de4e54a0f7862aa03260177cab589728
MortezaGhoddousi <143504966+MortezaGhoddousi@users.noreply.github.com>
2023-11-11 15:15:51 +0330

    Added session 15 to Web 15

---
+++ Web15/Session 15/script.js
@@ -0,0 +1,22 @@
+console.log("Hello from js file");
+
+var x = 10;
+
+console.log(x);
+
+var age = 15;
+
+var result1 = (age >= 18) ? "Adult" : "Teenager";
+console.log(result1);
+
+if (age >= 18) {
+    result2 = "Adult";
+} else {
+    result2 = "Teenager";
+}
+console.log(result2);
+
+var str1 = "Hello ";
+var str2 = "World!";
+
+console.log(str1 + str2);

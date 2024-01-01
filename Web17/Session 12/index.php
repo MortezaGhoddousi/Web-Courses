59dcc40d2968b6d1193c23c26424b7f38dd0dd93
MortezaGhoddousi <143504966+MortezaGhoddousi@users.noreply.github.com>
2024-01-01 16:17:28 +0330

    Added session 12 to Web 17

---
+++ Web17/Session 12/index.php
@@ -0,0 +1,33 @@
+<?php
+
+$firstName = 'Morteza';
+$lastName = 'Ghoddousi';
+$age = 29;
+
+
+?>
+
+
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Portfolio</title>
+    <link rel="stylesheet" href="./style.css">
+</head>
+<body>
+    <div>
+        <?php echo "<p>Hello guys!</p>"; ?>
+        <?php echo "<p>$firstName</p>"; ?>
+        <?php echo "<p>$lastName</p>"; ?>
+        <?php echo "<p>$age</p>"; ?>
+    </div>
+</body>
+
+</html>

039cd4359c675c3ae43a614f0f06a91d8865e545
MortezaGhoddousi <143504966+MortezaGhoddousi@users.noreply.github.com>
2024-04-07 18:46:06 +0330

    Added session 15 to Web 18

---
+++ Web18/Session 15/index.php
@@ -0,0 +1,27 @@
+<?php
+
+echo "Hello from PHP!";
+
+$fname = 'Mohammad';
+
+
+
+
+?>
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Welcome to My page</h1>
+    <p>This is the best page that you can find in the Internet!</p>
+    <?php echo $fname; ?>
+</body>
+</html>

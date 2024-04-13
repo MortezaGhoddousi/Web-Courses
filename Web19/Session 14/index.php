facd14d0fee4552269b52635a24261140ecc42ef
MortezaGhoddousi <143504966+MortezaGhoddousi@users.noreply.github.com>
2024-04-13 17:02:58 +0330

    Added session 14 to Web 19

---
+++ Web19/Session 14/index.php
@@ -0,0 +1,88 @@
+<?php
+
+// String
+$name = 'Morteza';
+$job = 'Developer';
+$summary = "I'm Morteza";
+
+// Integer
+$age = 30;
+
+// Float
+$Weight = 91.4;
+
+// Boolean
+$isMale = true;
+// echo true;
+
+
+$str = "10";
+$int = 20;
+$sum = $str + $int;
+echo ($sum);
+
+$x = 7;
+$y = 4;
+
+echo $x+$y;
+echo "<br/>";
+
+echo $x-$y;
+echo "<br/>";
+
+echo $x*$y;
+echo "<br/>";
+
+echo $x/$y;
+echo "<br/>";
+
+echo $x%$y;
+echo "<br/>";
+
+$x = 50;
+$x += 100; // $x = $x + 100
+echo $x;
+echo "<br/>";
+
+$x = 7;
+$y = 4;
+
+echo "$x == $y => ";
+echo $x == $y;
+echo "<br/>";
+
+echo "$x === $y => ";
+echo $x === $y;
+echo "<br/>";
+
+echo "$x != $y => ";
+echo $x != $y;
+echo "<br/>";
+
+echo "$x <> $y => ";
+echo $x <> $y;
+echo "<br/>";
+
+echo "$x !== $y => ";
+echo $x !== $y;
+echo "<br/>";
+
+echo "$x >= $y => ";
+echo $x >= $y;
+echo "<br/>";
+
+echo "$x <= $y => ";
+echo $x <= $y;
+echo "<br/>";
+
+echo "$x > $y => ";
+echo $x > $y;
+echo "<br/>";
+
+echo "$x < $y => ";
+echo $x < $y;
+echo "<br/>";
+
+
+
+?>

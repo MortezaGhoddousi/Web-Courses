9e7fcd9a303c057353838c6134423d4bf6c1ebf5
Morteza <143504966+MortezaGhoddousi@users.noreply.github.com>
2024-08-04 18:19:51 +0330

    Added session 11 to Web 22

---
+++ Web22/Session 11/index.php
@@ -0,0 +1,117 @@
+<?php
+
+echo "Morteza";
+echo 30;
+
+// String
+$firstName = "Morteza";
+// String
+$lastName = "Ghoddousi";
+// Integer
+$age = 30;
+// Float
+$height = 1.86;
+// Integer
+$weight = 92;
+// Boolean
+$isMale = true;
+
+echo $firstName;
+
+$x = 4;
+$y = 7;
+echo "<br/>";
+
+$z = $x + $y;
+echo "<h1>$x + $y = $z</h1>";
+
+$z = $x - $y;
+echo "<h1>$x - $y = $z</h1>";
+
+$z = $x * $y;
+echo "<h1>$x * $y = $z</h1>";
+
+$z = $x / $y;
+echo "<h1>$x / $y = $z</h1>";
+
+$z = $x % $y;
+echo "<h1>$x % $y = $z</h1>";
+
+
+// String
+$firstName = "Morteza";
+// String
+$lastName = "Ghoddousi";
+
+$fullName = $firstName.$lastName; //"MortezaGhoddousi"
+echo "<p>$fullName</p>";
+
+$i = 1;
+$i = 2;
+
+// $i = $i + 2;
+$i+=2;
+
+// $i = $i - 2;
+$i-=2;
+
+// $i = $i * 2;
+$i*=2;
+
+// $i = $i / 2;
+$i/=2;
+
+// $i = $i % 2;
+$i%=2;
+
+// $i = $i + 1;
+$i++;
+
+// $i = $i - 1;
+$i--;
+echo $i;
+
+?>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<!-- <!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <link rel="stylesheet" href="./style.css" />
+        <title>Document</title>
+    </head>
+    <body>
+        <article>
+            <p>
+                <?php echo "Morteza" ?>
+            </p>
+        </article>
+        <script src="./script.js"></script>
+    </body>
+</html> -->

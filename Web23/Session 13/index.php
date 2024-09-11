913ccec324a00b7bb55d430f351dbec0f240160f
Morteza <143504966+MortezaGhoddousi@users.noreply.github.com>
2024-09-11 16:00:48 +0330

    Added session 13 Web 23

---
+++ Web23/Session 13/index.php
@@ -0,0 +1,41 @@
+
+<?php 
+
+// string
+$fname = 'Morteza';
+$lname = "Ghoddousi";
+
+// integer
+$age = 30;
+$weight = 92;
+
+// float
+$height = 1.86;
+
+// boolean
+$isMale = true;
+
+echo "<h1>$fname</h1>";
+echo $lname;
+echo "<br />";
+echo $age;
+echo "<br />";
+echo $weight;
+echo "<br />";
+
+?>
+
+
+
+<!-- <!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <p>Morteza</p>
+</body>
+</html> -->

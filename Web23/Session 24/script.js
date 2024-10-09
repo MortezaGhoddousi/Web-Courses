1155e0669b028a12b812e4a119af2bfb1fb684c9
Morteza <143504966+MortezaGhoddousi@users.noreply.github.com>
2024-10-09 15:28:47 +0330

    Added session 24 to Web 23

---
+++ Web23/Session 24/script.js
@@ -0,0 +1,27 @@
+var x = 4;
+x = x + 1;
+
+var fname = "Morteza";
+var lname = "Ghoddousi";
+
+var age = 30;
+var height = 1.85;
+var weight = 92;
+
+var isMale = true;
+
+var myInformation = ["Morteza", "Ghoddousi", 30, 1.85, 92, true];
+
+console.log(fname);
+console.log(lname);
+console.log(age);
+console.log(height);
+console.log(weight);
+console.log(isMale);
+console.log(myInformation);
+
+for (var i = 0; i < 3; i++) {
+    console.log(i);
+    console.log("Morteza");
+}
+

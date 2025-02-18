05f5d808cb9b2b8972539a2bed65ec472d082c8e
Morteza <143504966+MortezaGhoddousi@users.noreply.github.com>
2025-02-18 18:40:04 +0330

    Added session 12 to Web 26

---
+++ Web26/Session 12/index.php
@@ -0,0 +1,50 @@
+<?php
+
+// string
+$fname = "Morteza";
+$lname = "Ghoddousi";
+// integer
+$age = 30;
+$weight = 90;
+// float
+$height = 1.86;
+// boolean
+$isMale = true;
+
+echo ("Your Name: ");
+echo ($fname);
+echo ($lname);
+echo ("<br />");
+echo ("Your Age: ");
+echo ($age);
+echo ("<br />");
+echo ("Your Weight: ");
+echo ($weight);
+echo ("<br />");
+echo ("Your Hieght: ");
+echo ($height);
+echo ("<br />");
+echo ("Are you Male: ");
+echo ($isMale);
+echo ("<br />");
+
+?>
+
+
+
+<!-- <!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <link rel="stylesheet" href="./style.css" />
+        <title>Document</title>
+    </head>
+    <body>
+        <article>
+            <h1>hello from my local host</h1>
+        </article>
+        <script src="./script.js"></script>
+    </body>
+</html> -->

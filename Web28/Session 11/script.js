8c28a65ab6964071e54d68681e7b7ed3452c505f
MortezaGhoddousi <djmgh1994@gmail.com>
2025-12-14 19:47:49 +0330

    added session 11 to Web 28

---
+++ Web28/Session 11/script.js
@@ -0,0 +1,62 @@
+// assigning operator
+// string
+var firstName = "Morteza";
+var lastName = "Ghoddousi";
+
+// integer
+var age = 31;
+var weight = 83;
+
+// float
+var height = 1.86;
+
+// boolean
+var isMale = true;
+
+// console
+console.log("Hello");
+
+console.log(firstName);
+
+console.log(`hello ${firstName}`);
+
+// mathematical operators
+
+var x = 4;
+var y = 3;
+
+var z = x + y;
+// console.log(x, "+", y, "=", z);
+// console.log("x + y");
+console.log(`${x} + ${y} = ${z}`);
+
+z = x - y;
+console.log(`${x} - ${y} = ${z}`);
+
+z = x * y;
+console.log(`${x} * ${y} = ${z}`);
+
+z = x / y;
+console.log(`${x} / ${y} = ${z}`);
+
+z = x % y;
+console.log(`${x} % ${y} = ${z}`);
+
+z = x ** y;
+console.log(`${x} ** ${y} = ${z}`);
+
+x = "string";
+z = x - 5;
+console.log(z);
+
+var i = 4;
+
+i += 3; // i = i + 3;
+console.log(i);
+i -= 3; // i = i - 3;
+i *= 3; // i = i * 3;
+i /= 3; // i = i / 3;
+i %= 3; // i = i % 3;
+i **= 3; // i = i ** 3;
+i++; // i = i+1; i+=1;
+i--; // i = i-1; i-=1;

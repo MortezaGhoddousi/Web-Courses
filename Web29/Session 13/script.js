18a4b33968f8d83660918e8f5b029970598af48f
MortezaGhoddousi <djmgh1994@gmail.com>
2026-02-15 18:49:40 +0330

    added session 13 to Web 29

---
+++ Web29/Session 13/script.js
@@ -0,0 +1,97 @@
+var firstname = "Morteza";
+var lastname = "Ghoddousi";
+var age = 31;
+var weight = 82;
+var height = 1.86;
+var isMale = true;
+
+console.log(firstname);
+console.log(lastname);
+console.log(age);
+console.log(weight);
+console.log(height);
+console.log(isMale);
+
+var x = 8;
+var y = 5;
+
+var z = x + y;
+console.log(z);
+
+z = x - y;
+console.log(z);
+
+z = x * y;
+console.log(z);
+
+z = x / y;
+console.log(z);
+
+z = x ** y;
+console.log(z);
+
+z = x % y;
+console.log(z);
+
+console.log(firstname + lastname);
+console.log(firstname / lastname);
+console.log(x * lastname);
+console.log(x + lastname);
+
+console.log(4 + "m");
+console.log("4" + 7);
+
+console.log(firstname);
+
+firstname = "Sarah";
+console.log(firstname);
+
+var i = 3;
+i = i + 5;
+i += 5;
+
+i = i - 5;
+i -= 5;
+
+i = i * 5;
+i *= 5;
+
+i = i / 5;
+i /= 5;
+
+i = i % 5;
+i %= 5;
+
+i = i ** 5;
+i **= 5;
+
+i = i + 1;
+i += 1;
+i++;
+
+i = i - 1;
+i -= 1;
+i--;
+
+console.log("5 < 7", 5 < 7);
+console.log("5 <= 7", 5 <= 7);
+console.log("5 > 7", 5 > 7);
+console.log("5 >= 7", 5 >= 7);
+console.log("5 == 7", 5 == 7);
+console.log("5 != 7", 5 != 7);
+console.log("5 === 7", 5 === 7);
+console.log("5 !== 7", 5 !== "7");
+
+console.log("true && true", true && true);
+console.log("true && false", true && false);
+console.log("false && true", false && true);
+console.log("false && false", false && false);
+
+console.log("true || true", true || true);
+console.log("true || false", true || false);
+console.log("false || true", false || true);
+console.log("false || false", false || false);
+
+console.log("!false", !false);
+console.log("!true", !true);
+
